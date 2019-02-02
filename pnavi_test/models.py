82192f5db4d54290b7b983b51b991ded4cfaa690
Allen Wong <wangxl1029@sina.com>
2019-02-02 13:48:57 +0800

    New feature: add the models of poll and p-nav.

--- pnavi_test/models.py
+++ pnavi_test/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class TestTask(models.Model):
+    test_title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class TestItem(models.Model):
+    task = models.ForeignKey(TestTask, on_delete=models.CASCADE)
+    item_text = models.CharField(max_length=200)
+    is_checked = models.BooleanField(default=False)
--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is poll index.')
+    return HttpResponse("Hello, world. You're at the polls index.")
