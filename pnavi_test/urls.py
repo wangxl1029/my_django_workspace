60424dd3b026149760c647c86d671f360ef2d7a1
Allen Wong <wangxl1029@sina.com>
2019-02-02 16:17:05 +0800

    New feature: add detail page as test item to the p-nav.

--- pnavi_test/urls.py
+++ pnavi_test/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, detail
 
 urlpatterns = [
-    path('', index, name='pnavi')
+    path('', index, name='pnavi'),
+    path('<int:task_id>/', detail, name='detail')
 ]
--- pnavi_test/views.py
+++ pnavi_test/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('This is predictive navigation page.')
+
+
+def detail(request, task_id):
+    return HttpResponse('This is the test task %d' % task_id)
