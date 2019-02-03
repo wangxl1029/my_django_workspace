11b92c01e2a692288caec8ca087fd7364b8b4715
Allen Wong <wangxl1029@sina.com>
2019-02-03 12:55:25 +0800

    [PNAVI][VIEW]:use shortcut to render template.

--- pnavi_test/views.py
+++ pnavi_test/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+
 from .models import TestTask
 
 
 # Create your views here.
 def index(request):
     latest_task_list = TestTask.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('index.html')
     context = {
         'latest_task_list': latest_task_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 
 def detail(request, task_id):
