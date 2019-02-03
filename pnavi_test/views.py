32aed59cc9eff65110cdc2dbbb31f3b90cf73b2c
Allen Wong <wangxl1029@sina.com>
2019-02-03 11:25:42 +0800

    [PNAVI][DB]:use DB. 1) 注册数据库到管理员页。 2) 实现数据库的str方法。 3) 视图中打印数据库的前几项。

--- pnavi_test/views.py
+++ pnavi_test/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TestTask
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is predictive navigation page.')
+    latest_task_list = TestTask.objects.order_by('-pub_date')[:5]
+    output = ','.join(t.test_title for t in latest_task_list)
+    return HttpResponse(output)
 
 
 def detail(request, task_id):
