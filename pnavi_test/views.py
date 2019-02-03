029db1907e17ec61a97f6c2aeeca3d50ac9efff1
Allen Wong <wangxl1029@sina.com>
2019-02-03 12:25:13 +0800

    [PNAVI][TEMPLATE]: add and use the template of index.

--- pnavi_test/views.py
+++ pnavi_test/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import TestTask
 
 
 # Create your views here.
 def index(request):
     latest_task_list = TestTask.objects.order_by('-pub_date')[:5]
-    output = ','.join(t.test_title for t in latest_task_list)
-    return HttpResponse(output)
+    template = loader.get_template('index.html')
+    context = {
+        'latest_task_list': latest_task_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, task_id):
