1a3fd227c0ff4c52dab445b93f594ff6c91a272c
Allen Wong <wangxl1029@sina.com>
2019-02-05 15:55:18 +0800

    [POLL][ADMIN]:put upside down the fields in admin page.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+
 from .models import Question
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
