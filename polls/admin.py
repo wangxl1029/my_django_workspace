37e22d3560dfb6f3342b1a0833928c33d0226b1e
Allen Wong <wangxl1029@sina.com>
2019-02-05 16:12:53 +0800

    [POLL][ADMIN]: associate the choices to question as inline style.

--- polls/admin.py
+++ polls/admin.py
@@ -3,13 +3,18 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ("Date information", {'fields': ['pub_date']})
     ]
+    inlines = [ChoiceInLine]
 
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
