7ff2b6fa944035604d079f808cb236c938b594fd
Allen Wong <wangxl1029@sina.com>
2019-02-08 11:32:16 +0800

    [IMG][UPLOADER][VIEWS]: add update view.

--- img_uploader/views.py
+++ img_uploader/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Image
+
 
 # Create your views here.
+def uploadImg(request):
+    """
+    图片上传
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        new_img = Image(
+            img=request.FILES.get('img'),
+            name=request.FILES.get('img').name
+        )
+        new_img.save()
+    return render(request, 'img_uploader/uploading.html')
+
--- my_workspace/settings.py
+++ my_workspace/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'img_uploader.apps.ImgUploaderConfig',
     'pnavi_test.apps.PnaviTestConfig',
 #   'polls.apps.PollsConfig',
     'django.contrib.admin',
