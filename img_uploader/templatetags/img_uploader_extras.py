10a457a6488919ed98fa615ff2ade29cacfc6fe2
Allen Wong <wangxl1029@sina.com>
2019-02-23 15:00:16 +0800

    [IMG][UPLOADER][refactor]: set max to the image height.

--- img_uploader/templatetags/img_uploader_extras.py
+++ img_uploader/templatetags/img_uploader_extras.py
@@ -12,3 +12,8 @@ def current_time(format_string):
 @register.inclusion_tag("img_uploader/includes/tag_line.html")
 def show_tag_line(image):
     return {'img': image}
+
+
+@register.filter
+def img_max_h(h, max_h):
+    return '' if h < max_h else "height=%d" % max_h
